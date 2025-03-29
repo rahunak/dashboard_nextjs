b6eae07060ef6b6d4f803b89121a5048536a1495
Eugeny Zaiko <e.zaiko@syberry.com>
2025-03-29 19:21:52 +0300

    Added Suspend functionality

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,3 +1,9 @@
-export default function Page() {
+async function name() {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+ }
+
+export default async function Page() {
+
+ await name() ;
   return <p>invoices Page</p>;
 }
---
+++ app/seed/ReadMe.md
@@ -0,0 +1,5 @@
+this file was used to pollute database.
+Pollute start with open url: http://localhost:3000/seed
+
+Database is available here:
+https://supabase.com/dashboard/project/[env->POSTGRES_HOST(2rd part)]/editor
